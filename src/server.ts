8396ef914bd4eba1a9f4e8e1dc787713e619f3fd
Jesse Jonas <jessejonas13@gmail.com>
2021-01-13 11:57:21 -0300

    adicionado cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -23,7 +25,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
-  console.error(err);
+  console.error(err); // eslint-disable-line
 
   return response.status(500).json({
     status: 'error',
